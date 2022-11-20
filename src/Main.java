d463d2bad273cd85dcc78724064443723c05ac60
Sargeanthost <sargeanthost@gmail.com>
2022-11-20 04:19:41 -0500

    fast inversion

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Palindromes");
         palindrome();
+        System.out.println("Slow inversion");
         easyInversion();
+        System.out.println("Fast inversion");
+        fastInversion();
     }
 
     public static void palindrome() {
@@ -14,9 +18,26 @@ public class Main {
     public static void easyInversion() {
         int[] testSet = {3, 2, 1};
         int[] testSet2 = {1, 2, 3};
-        int[] testSet3 = {1,3,2};
-        System.out.println("Number of inversions: " + easyinversioncount.easyInversion(testSet));
-        System.out.println("Number of inversions: " + easyinversioncount.easyInversion(testSet2));
-        System.out.println("Number of inversions: " + easyinversioncount.easyInversion(testSet3));
+        int[] testSet3 = {1, 3, 2};
+        int[] testSet4 = {8, 4, 2, 1};
+        int[] testSet5 = {20, 9, 1, 15, 10, 7};
+        System.out.println("Number of slow inversions: " + easyinversioncount.easyInversion(testSet));
+        System.out.println("Number of slow inversions: " + easyinversioncount.easyInversion(testSet2));
+        System.out.println("Number of slow inversions: " + easyinversioncount.easyInversion(testSet3));
+        System.out.println("Number of slow inversions: " + easyinversioncount.easyInversion(testSet4));
+        System.out.println("Number of slow inversions: " + easyinversioncount.easyInversion(testSet5));
+    }
+
+    public static void fastInversion(){
+        int[] testSet = {3, 2, 1};
+        int[] testSet2 = {1, 2, 3};
+        int[] testSet3 = {1, 3, 2};
+        int[] testSet4 = {8, 4, 2, 1};
+        int[] testSet5 = {20, 9, 1, 15, 10, 7};
+        System.out.println("Number of fast inversions: " + fastinversioncount.fastInversion(testSet));
+        System.out.println("Number of fast inversions: " + fastinversioncount.fastInversion(testSet2));
+        System.out.println("Number of fast inversions: " + fastinversioncount.fastInversion(testSet3));
+        System.out.println("Number of fast inversions: " + fastinversioncount.fastInversion(testSet4));
+        System.out.println("Number of fast inversions: " + fastinversioncount.fastInversion(testSet5));
     }
 }
--- src/fastinversioncount.java
+++ src/fastinversioncount.java
@@ -1,6 +1,68 @@
+import java.util.Arrays;
+
 public class fastinversioncount {
-    public static int fastInversion(int[] arr){
+    // Merge count will work alongside a merge sort. There are two steps, partition and merge. When partitioning,
+    // there are no extra steps; it is the same as merge sort. When merging, we count the inversions in the current
+    // bucket. For an even numbered starting array, this will be something like [20,1] (1 inversion present). Then,
+    // they will be sorted. This guarantees that when merging with another array, we only have to go up until a
+    // single inversion is found, then all remaining elements will have an inversion with the current number. If not,
+    // then it will go through the entire to-merge array (worst case). Good case example:
+    // I([20,1]) => 1 | I([12,3]) => 2 ==> M([1,20],[3,12]) => I([1,3]) => 0 | I([1,12]) => 0 / I([20,3]) => 1 | I([20,
+    // 12]) => 1.
+    // Short circuit example:
+    //  M([20,21],[6,7]) => I([20,6]) => 1. Since anything to the right of 20 is greater, we add I(x) * 2, so there
+    //  is no need to check 21 for any case.
+
+    /**
+     * Counts the inversions between two ordered arrays.
+     *
+     * @param arr   left array
+     * @param left  pointer to left array elem
+     * @param mid   pointer to right array elem
+     * @param right temp pointer
+     * @return returns inversions
+     */
+    private static int merge(int[] arr, int left, int mid, int right) {
+        int[] leftA = Arrays.copyOfRange(arr, left, mid + 1);
+        int[] rightA = Arrays.copyOfRange(arr, mid + 1, right + 1);
         int count = 0;
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        while (i < leftA.length && j < rightA.length) {
+            if (leftA[i] <= rightA[j]) {
+                arr[k++] = leftA[i++];//ordered
+            } else {
+                arr[k++] = rightA[j++];//inversion
+                count += (mid + 1) - (left + i);
+            }
+        }
+        while (i < leftA.length)
+            arr[k++] = leftA[i++];
+        while (j < rightA.length)
+            arr[k++] = rightA[j++];
         return count;
+    }
+
+    //recursive function. base case when there is only one number left
+    private static int fastInversion(int[] arr, int left, int right) {
+        int count = 0;
+
+        // if not arr of n = 1, then it is not split into base components of merge sort yet
+        if (right > left) {
+            int mid = (left + right) / 2;
+
+            count += fastInversion(arr, left, mid); //left
+            count += fastInversion(arr, mid + 1, right); //right
+
+            count += merge(arr, left, mid, right); // merge sub arrs
+        }
+        return count;
+    }
+
+    public static int fastInversion(int[] arr) {
+        //arr, temp array for merge, index of first elem, index of last elem
+        return fastInversion(arr, 0, arr.length - 1);
     }
 }
